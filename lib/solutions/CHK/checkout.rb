2f855e680858586ada439a3f7ecea4bd24ada955
sbx_user1051 <sbx_user1051@169.254.84.37>
2019-04-10 14:09:11 +0000

    Wed Apr 10 14:09:11 UTC 2019

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -1,16 +1,19 @@
 # noinspection RubyUnusedLocalVariable
 class Checkout
   def initialize(prices)
-    @prices = prices  
+    @prices = prices
   end
 
   def checkout(skus)
-    if skus == 'A'
-      return 50
-    else
-      return 30
-    end
+    # if skus == 'A'
+    #   return 50
+    # else
+    #   return 30
+    # end
+
+    return @prices[skus]
   end
 
 end
+
 
--- spec/CHK/checkout_spec.rb
+++ spec/CHK/checkout_spec.rb
@@ -1,6 +1,16 @@
 require './lib/solutions/CHK/checkout.rb'
 
+prices = {
+  'A' => 50,
+  'B' => 30,
+  'C' => 20,
+  'D' => 15
+}
+
 describe Checkout do
+
+  subject { Checkout.new(prices) }
+
   it "prices up A" do
     expect(subject.checkout('A')).to eq(50)
   end
@@ -12,5 +22,10 @@ describe Checkout do
   it "prices up C" do
     expect(subject.checkout('C')).to eq (20)
   end
+
+  it "prices up D" do
+    expect(subject.checkout('D')).to eq (15)
+  end
 end
+
 
