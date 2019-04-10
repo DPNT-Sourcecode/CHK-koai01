def7cfd46a19863cb7a428fa9ee78ae5eac08e3b
sbx_user1051 <sbx_user1051@169.254.84.37>
2019-04-10 14:15:11 +0000

    Wed Apr 10 14:15:11 UTC 2019

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -5,11 +5,22 @@ class Checkout
   end
 
   def checkout(skus)
-    return -1 if @prices[skus].nil?
-    @prices[skus]
+    # return -1 if @prices[skus].nil?
+    # @prices[skus]
+
+    # skus.split('').each { return -1 if @prices[skus].nil? }
+
+    items_array = skus.split('')
+    p items_array
+    items_array.map! { |x| @prices[x] }
+    p items_array
+    items_array.each { |x| return -1 if x.nil? }
+    p items_array
+    return items_array.reduce(:+)
   end
 
 end
+
 
 
 
--- spec/CHK/checkout_spec.rb
+++ spec/CHK/checkout_spec.rb
@@ -11,23 +11,23 @@ describe Checkout do
 
   subject { Checkout.new(prices) }
 
-  context "prices individual items" do
-    it "prices up A" do
-      expect(subject.checkout('A')).to eq(50)
-    end
-
-    it "prices up B" do
-      expect(subject.checkout('B')).to eq (30)
-    end
-
-    it "prices up C" do
-      expect(subject.checkout('C')).to eq (20)
-    end
-
-    it "prices up D" do
-      expect(subject.checkout('D')).to eq (15)
-    end
-  end
+  # context "prices individual items" do
+  #   it "prices up A" do
+  #     expect(subject.checkout('A')).to eq(50)
+  #   end
+  #
+  #   it "prices up B" do
+  #     expect(subject.checkout('B')).to eq (30)
+  #   end
+  #
+  #   it "prices up C" do
+  #     expect(subject.checkout('C')).to eq (20)
+  #   end
+  #
+  #   it "prices up D" do
+  #     expect(subject.checkout('D')).to eq (15)
+  #   end
+  # end
 
   context "detects invalid items" do
     it "returns -1 when a single invalid item passed in" do
@@ -35,13 +35,14 @@ describe Checkout do
     end
   end
 
-  context "prices up multiple items" do
-    it "prices up AB" do
-      expect(subject.checkout('AB')).to eq (80)
-    end
-  end
+  # context "prices up multiple items" do
+  #   it "prices up AB" do
+  #     expect(subject.checkout('AB')).to eq (80)
+  #   end
+  # end
 
 end
+
 
 
 
