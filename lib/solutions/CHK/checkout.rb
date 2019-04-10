576b0c540caa652326055fed359f60c93a67f5a2
sbx_user1051 <sbx_user1051@169.254.84.37>
2019-04-10 14:06:11 +0000

    Wed Apr 10 14:06:11 UTC 2019

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -1,8 +1,16 @@
 # noinspection RubyUnusedLocalVariable
 class Checkout
+  def initialize(prices)
+    @prices = prices  
+  end
 
   def checkout(skus)
-    raise 'Not implemented'
+    if skus == 'A'
+      return 50
+    else
+      return 30
+    end
   end
 
 end
+
--- spec/CHK/checkout_spec.rb
+++ spec/CHK/checkout_spec.rb
@@ -1,7 +1,16 @@
 require './lib/solutions/CHK/checkout.rb'
 
 describe Checkout do
-  it "responds to this test" do
-    expect(subject.checkout('skus')).to raise_error('Not implemented')
+  it "prices up A" do
+    expect(subject.checkout('A')).to eq(50)
+  end
+
+  it "prices up B" do
+    expect(subject.checkout('B')).to eq (30)
+  end
+
+  it "prices up C" do
+    expect(subject.checkout('C')).to eq (20)
   end
 end
+
