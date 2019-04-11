2298072cc8f045fd81b01bba8da00b271b9ccff0
sbx_user1051 <sbx_user1051@169.254.16.69>
2019-04-11 16:21:12 +0000

    Thu Apr 11 16:21:12 UTC 2019

--- lib/solutions/CHK/checkout.rb
+++ lib/solutions/CHK/checkout.rb
@@ -63,14 +63,17 @@ class Checkout
   def price_multi_item_bundles
     multi_items = []
     @basket.items.each do |item|
-      multi_items.push @basket.items.delete_at(@basket.items.index(item))
+      if @deals.cross_item_deals.include?(item)
+        multi_items << item 
+        @basket.items.delete_at(@basket.items.index(item))
     end
-
+    p multi_items
   end
 
   def valid_basket
     @basket.items.each { |x| return false unless @prices.pricelist.key?(x) }
   end
 end
+
 
 
