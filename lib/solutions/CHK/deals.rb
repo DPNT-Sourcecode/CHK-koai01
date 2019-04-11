a02bdb85fcc46d0d4a9d0d511a2189832786a03a
sbx_user1051 <sbx_user1051@169.254.185.93>
2019-04-11 14:08:09 +0000

    Thu Apr 11 14:08:09 UTC 2019

--- lib/solutions/CHK/deals.rb
+++ lib/solutions/CHK/deals.rb
@@ -6,7 +6,12 @@ class Deals
       {'item' => 'A', 'quantity' => 5, 'price' => 200, 'bonus_item' => nil, 'bonus_item_quantity' => nil, 'bonus_item_price' => nil},
       {'item' => 'A', 'quantity' => 3, 'price' => 130, 'bonus_item' => nil, 'bonus_item_quantity' => nil, 'bonus_item_price' => nil},
       {'item' => 'B', 'quantity' => 2, 'price' => 45, 'bonus_item' => nil, 'bonus_item_quantity' => nil, 'bonus_item_price' => nil},
-      {'item' => 'F', 'quantity' => 3, 'price' => 20, 'bonus_item' => nil, 'bonus_item_quantity' => nil, 'bonus_item_price' => nil}
+      {'item' => 'F', 'quantity' => 3, 'price' => 20, 'bonus_item' => nil, 'bonus_item_quantity' => nil, 'bonus_item_price' => nil},
+      {'item' => 'H', 'quantity' => 10, 'price' => 80, 'bonus_item' => nil, 'bonus_item_quantity' => nil, 'bonus_item_price' => nil},
+      {'item' => 'H', 'quantity' => 5, 'price' => 45, 'bonus_item' => nil, 'bonus_item_quantity' => nil, 'bonus_item_price' => nil},
+      {'item' => 'K', 'quantity' => 2, 'price' => 150, 'bonus_item' => nil, 'bonus_item_quantity' => nil, 'bonus_item_price' => nil},
+      {'item' => 'P', 'quantity' => 5, 'price' => 200, 'bonus_item' => nil, 'bonus_item_quantity' => nil, 'bonus_item_price' => nil}
     ]
   end
 end
+
